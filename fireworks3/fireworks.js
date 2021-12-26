4960908f4dccecd46e552ccf42e9640738802126
Xiaoleng123 <896054682@qq.com>
2021-12-27 00:13:02 +0800

    烟花第三版：一起跨年吧！

---
+++ fireworks3/fireworks.js
@@ -0,0 +1,312 @@
+const defaultColors = [210, 270, 255, 30, 285].map(item => `hsla(${item}, 100%, 50%, 1)`)
+
+// 焰火集合类
+class Fireworks {
+  _timer = null
+  _animater = null
+  _useAnimationFrame = true
+  
+  ctx = null // 画布上下文，都画这上面
+  fps = 40 // 帧率控制
+  fireworks = [] // 焰火数组
+  fireworkCount = 10 // 焰火数量
+  fireworkInterval = 300 // 焰火爆炸间隔💥
+  fireworkColors = defaultColors // 焰火颜色随机取值数组
+  particleOptions = { // 粒子配置
+    size: 15, // 几块钱的烟花
+    speed: 15, // 燃烧的速度
+    gravity: 0.08, // 🌍 地球的引力，向下的
+    power: 0.93, // 动力，值越大冲越远
+    shrink: 0.96, // 燃料消耗的速度
+    jitter: 1, // 摇摇晃摇
+    color: 'hsla(210, 100%, 50%, 1)', // 颜色
+  }
+
+  constructor(dom, options = {}) {
+    if (!(dom instanceof HTMLElement)) {
+      options = dom || {}
+    }
+
+    if (!dom) {
+      dom = document.body
+    }
+
+    this.initCanvas(dom)
+
+    const { particleOptions = {}, ...others } = options
+    this.particleOptions = { ...this.particleOptions, ...particleOptions }
+    Object.keys(others).forEach(key => this[key] = others[key])
+
+    this._useAnimationFrame = this.fps >= 60
+  }
+
+  // 初始化画布
+  initCanvas(dom) {
+    let canvas = dom
+
+    const isCanvas = canvas.nodeName.toLowerCase() === 'canvas'
+    if (!isCanvas) {
+      canvas = document.createElement('canvas')
+      dom.appendChild(canvas)
+    }
+
+    const { width, height } = dom.getBoundingClientRect()
+    canvas.width = width
+    canvas.height = height
+    canvas.style.cssText = `width: ${width}px; height: ${height}px;`
+
+    this.ctx = canvas.getContext('2d')
+  }
+
+  // 创建单个焰火
+  createFirework() {
+    const { width, height } = this.ctx.canvas
+    const x = random(width * 0.1, width * 0.9)
+    const y = random(height * 0.1, height * 0.9)
+    const color = random(this.fireworkColors)
+    const firework = new Firework({ ctx: this.ctx, ...this.particleOptions, x, y, color })
+    this.fireworks.push(firework)
+  }
+
+  // 焰火燃尽，无情灭之
+  checkFireworks() {
+    const indexArray = []
+    this.fireworks.forEach((firework, index) => {
+      if (firework.isBurnOff()) {
+        indexArray.push(index)
+      }
+    })
+    indexArray.reverse().forEach(index => this.fireworks.splice(index, 1))
+  }
+
+  // 检查是否需要创建焰火
+  loop() {
+    let interval = this.fireworkInterval * random(0.5, 1)
+    this._timer = setTimeout(() => {
+      this.checkFireworks()
+
+      if (this.fireworks.length < this.fireworkCount) {
+        this.createFirework()
+      }
+
+      this.loop()
+    }, interval)
+  }
+
+  // 绘制焰火
+  render() {
+    // 60 帧就用 requestAnimationFrame，否则用 setTimeout
+    const animationFunction = this._useAnimationFrame ? requestAnimationFrame : setTimeout
+    const interval = 16.67 * (60 / this.fps)
+    this._animater = animationFunction(() => {
+      // 通过绘制黑色透明图层，达到尾焰的效果
+      this.ctx.fillStyle = 'rgba(0, 0, 0, 0.05)'
+      const { width, height } = this.ctx.canvas
+      this.ctx.fillRect(0, 0, width, height)
+
+      this.fireworks.forEach(firework => {
+        firework.render()
+      })
+      this.render()
+    }, interval)
+  }
+
+  // 前进四 ！！！
+  start() {
+    this.loop()
+    this.render()
+  }
+
+  // 休息一下
+  pause() {
+    this._timer && clearTimeout(this._timer)
+    this._animater && (this._useAnimationFrame ? cancelAnimationFrame(this._animater)
+      : clearTimeout(this._animater))
+
+    this._timer = null
+    this._animater = null
+  }
+
+  // 结束吧这个世界
+  stop() {
+    this.pause()
+
+    this.fireworks.length = 0
+
+    const { width, height } = this.ctx.canvas()
+    this.ctx.clearRect(0, 0, width, height)
+  }
+}
+
+
+const STATUS = {
+  HEALTH: 'health',
+  BURN_OFF: 'burnOff'
+}
+// 焰火类
+class Firework {
+  status = STATUS.HEALTH
+
+  ctx = null
+  x = 0
+  y = 0
+
+  particleCount = 80
+  particles = []
+  particleOptions = {}
+
+  constructor(options = {}) {
+    const { ctx, x, y, particleCount = 80, ...particleOptions } = options
+    this.ctx = ctx
+    this.x = x
+    this.y = y
+    this.particleCount = particleCount
+    this.particleOptions = particleOptions
+
+    this.initParticles()
+  }
+
+  // 初始化粒子
+  initParticles() {
+    const { x, y, particleOptions } = this
+    const { size: pSize } = particleOptions
+    for (let index = 0; index < this.particleCount; index++) {
+      const shrink = random() * 0.05 + particleOptions.shrink
+      const size = random(-pSize / 2, pSize / 2) + pSize
+      const particle = new Particle({ x, y, shrink, size, ...particleOptions })
+      this.particles.push(particle)
+    }
+  }
+
+  // 更新粒子
+  updateParticles() {
+    const indexArray = []
+    this.particles.forEach((particle, index) => {
+      particle.update()
+      if (particle.isBurnOff()) {
+        indexArray.push(index)
+      }
+    })
+
+    indexArray.reverse().forEach(index => this.particles.splice(index, 1))
+
+     // 拥有的粒子都燃尽了，自己也就结束了
+    if (this.particles.length === 0) {
+      this.status = STATUS.BURN_OFF
+    }
+  }
+
+  // 渲染粒子
+  render() {
+    this.updateParticles()
+    if (this.isBurnOff()) return
+
+    this.particles.forEach(particle => {
+      particle.render(this.ctx)
+    })
+  }
+
+  isBurnOff() {
+    return this.status === STATUS.BURN_OFF
+  }
+}
+
+// 焰火粒子类
+class Particle {
+  size = 10
+  speed = 15
+  gravity = 0.2
+  power = 0.92
+  shrink = 0.93
+  jitter = 0.08
+  color = 'hsla(210, 100%, 50%, 1)'
+  shadowColor = 'hsla(210, 100%, 50%, 0.1)'
+
+  x = 0 // x 坐标位置
+  y = 0 // y 坐标位置
+
+  vel = { // 速度
+    x: 0,
+    y: 0,
+  }
+
+  constructor(options) {
+    Object.keys(options).forEach(key => {
+      this[key] = options[key]
+    })
+    const angle = random() * Math.PI * 2
+    const speed = Math.cos((random() * Math.PI) / 2) * this.speed
+    this.vel = {
+      x: Math.cos(angle) * speed,
+      y: Math.sin(angle) * speed,
+    }
+    this.shadowColor = tinycolor(this.color).setAlpha(0.1)
+  }
+
+  // 移形换位
+  update() {
+    this.vel.x *= this.power
+    this.vel.y *= this.power
+
+    this.vel.y += this.gravity
+
+    const jitter = random(-1, 1) * this.jitter
+    this.x += this.vel.x + jitter
+    this.y += this.vel.y + jitter
+
+    this.size *= this.shrink
+  }
+
+  // 绘制单粒子
+  render(ctx) {
+    if (this.isBurnOff()) return
+
+    ctx.save()
+
+    ctx.globalCompositeOperation = 'lighter'
+
+    // 绘制阴影性能损耗太大，顶不住，砍需求！
+    // ctx.shadowColor = 'rgba(255,255,255,0.6)'
+    // ctx.shadowBlur = 5
+
+    const { x, y } = this
+    const radius = this.size / 2
+
+    // 红里透白，像极了爱情
+    const gradient = ctx.createRadialGradient(x, y, 0.1, x, y, radius)
+    gradient.addColorStop(0.1, 'rgba(255,255,255,0.5)')
+    gradient.addColorStop(0.8, this.color)
+    gradient.addColorStop(1, this.shadowColor)
+
+    ctx.fillStyle = gradient
+
+    ctx.beginPath()
+    ctx.arc(x, y, this.size, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.fill()
+
+    ctx.restore()
+  }
+
+  // 小到看不到
+  isBurnOff() {
+    return this.size < 1
+  }
+}
+
+// 缝合怪随机函数，业务代码勿用，容易被砍
+function random(numbers, ...args) {
+  if (!numbers) {
+    return Math.random()
+  }
+  if (Array.isArray(numbers)) {
+    return numbers[Math.floor(Math.random() * numbers.length)]
+  }
+  if (args.length > 1) {
+    numbers = [numbers].concat(args)
+    return numbers[Math.floor(Math.random() * numbers.length)]
+  }
+  const min = numbers
+  const max = args[0]
+  return Math.random() * (max - min) + min
+}
---
+++ fireworks3/word.js
@@ -0,0 +1,194 @@
+class Word {
+  _loadImagePromise = null
+  _animater = null
+  _timer = null
+  _scale = 0.1
+  _maxScale = 1
+  _step = 0.01
+
+  ctx = null
+  image = null
+  stars = []
+
+  constructor(dom, image) {
+    this.loadImage(image)
+    this._loadImagePromise.then(() => {
+      this.initCanvas(dom)
+    })
+  }
+
+  initCanvas(dom) {
+    let canvas = dom
+
+    const isCanvas = canvas.nodeName.toLowerCase() === 'canvas'
+    if (!isCanvas) {
+      canvas = document.createElement('canvas')
+      dom.appendChild(canvas)
+    }
+
+    const { width, height } = this.image
+    canvas.width = width
+    canvas.height = height
+    canvas.style.cssText = `width: ${width}px; height: ${height}px;`
+
+    this.ctx = canvas.getContext('2d')
+  }
+
+  loadImage(image) {
+    this._loadImagePromise = new Promise((resolve, reject) => {
+      let imageElement = image
+      if (!(imageElement instanceof HTMLElement)) {
+        imageElement = new Image()
+        imageElement.src = image
+      }
+      imageElement.onload = function() {
+        resolve(this)
+      }
+      imageElement.onerror = function(error) {
+        reject(error)
+      }
+
+      this.image = imageElement
+    })
+  }
+
+  start() {
+    this._loadImagePromise.then(() => {
+      this.createStars()
+      this.render()
+    })
+  }
+
+  stop() {
+    this.clearCtx()
+
+    this._animater && cancelAnimationFrame(this._animater)
+    this._animater = null
+  }
+
+  createStars() {
+    const { width, height } = this.ctx.canvas
+    for (let x = 0; x < width; x += 12) {
+     for (let y = 0; y < height; y += 12) {
+        const star = new Star({ x, y })
+        this.stars.push(star)
+      }
+    }
+  }
+
+  render() {
+    this._animater = requestAnimationFrame(() => {
+      if (this._scale < this._maxScale) {
+        this.ctx.canvas.style.transform = `scale(${this._scale})`
+        this._scale += this._step
+      }
+
+      this.clearCtx()
+
+      this.renderImage()
+      this.renderStars()
+
+      this.render()
+    })
+  }
+
+  clearCtx() {
+    const { ctx } = this
+    const { width, height } = ctx.canvas
+    ctx.clearRect(0, 0, width, height)
+  }
+  
+  renderImage() {
+    const { ctx } = this
+
+    const { width, height } = ctx.canvas
+    ctx.drawImage(this.image, 0, 0, width, height)
+  }
+
+  updateStars() {
+    const indexArray = []
+    this.stars.forEach((star, index) => {
+      star.update()
+      if (star.isBurnOff()) {
+        indexArray.push(index)
+      }
+    })
+
+    indexArray.reverse().forEach(index => this.stars.splice(index, 1))
+
+    if (this.stars.length === 0) {
+      this.stop()
+    }
+  }
+
+  renderStars() {
+    this.updateStars()
+
+    // 离屏 canvas，以便使用 globalCompositeOperation
+    const { width, height } = this.ctx.canvas
+    const canvas = document.createElement('canvas')
+    canvas.width = width
+    canvas.height = width
+    const ctx = canvas.getContext('2d')
+    this.stars.forEach(star => {
+      star.render(ctx)
+    })
+
+    this.ctx.save()
+    this.ctx.globalCompositeOperation = 'source-in'
+    this.ctx.drawImage(canvas, 0, 0, width, height)
+    this.ctx.restore()
+  }
+}
+
+class Star {
+  x = 0
+  y = 0
+  size = 3
+  maxSize = 10
+  color = 'rgba(240, 230, 210, 0.8)'
+  shrink = 0.99
+
+  constructor(options = {}) {
+    Object.keys(options).forEach(key => {
+      this[key] = options[key]
+    })
+    this.shadowColor = tinycolor(this.color).setAlpha(0.1)
+  }
+
+  update() {
+    this.size /= this.shrink
+
+    if (this.size >= this.maxSize) {
+      this.shrink = 1 / this.shrink
+    }
+  }
+
+  render(ctx) {
+    if (this.isBurnOff()) return
+
+    ctx.save()
+
+    const { x, y } = this
+    const radius = this.size / 2
+
+    // const gradient = ctx.createRadialGradient(x, y, 0.1, x, y, radius)
+    // gradient.addColorStop(0.1, 'rgba(255,255,255,0.5)')
+    // gradient.addColorStop(0.8, this.color)
+    // gradient.addColorStop(1, this.shadowColor)
+
+    // ctx.fillStyle = gradient
+    ctx.fillStyle = this.color
+
+    ctx.beginPath()
+    ctx.arc(x, y, this.size, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.fill()
+
+    ctx.restore()
+  }
+
+  isBurnOff() {
+    return this.size < 1.2
+  }
+}
